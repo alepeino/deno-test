ac96f80e6db421f562c6edd008cba7da3b32c042
Alejandro Peinó <alepeino@gmail.com>
2022-05-28 11:56:25 -0300

    fix react type import

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 /** @jsx React.createElement */
 import React from "react";
-import type { FC } from "react";
+import type { FC } from "@t/react";
 
 export const App: FC = () => {
   return <h1>React SSR w/Deno</h1>;
